21bf0370e831d9d40d299f3098dcc73f7a03ae7f
Nathan Rihet <50133907+NathanKneT@users.noreply.github.com>
2024-11-28 09:48:40 +0100

    init

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,11 +8,11 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-jodie/gatsby-config.mjs
-    siteTitle: `Jodie`,
-    siteTitleAlt: `Jodie - Gatsby Starter Portfolio`,
-    siteHeadline: `Jodie - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://jodie.lekoarts.de`,
-    siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
+    siteTitle: `@nathanglhf`,
+    siteTitleAlt: `@nathanglhf - Urban portrait photographer`,
+    siteHeadline: `@nathanglhf - Urban portrait photographer`,
+    siteUrl: `https://nathanglhf.portfolio.com`,
+    siteDescription: `Photography portfolio about my works.`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
     author: `@lekoarts_de`,
