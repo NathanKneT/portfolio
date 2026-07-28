9e626bc99945a39333683d84e51660170081b268
Nathan Rihet <nathan.rihet06@gmail.com>
2026-07-28 23:04:55 +0900

    Add Google Search Console verification

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -3,6 +3,11 @@ import * as React from "react"
 
 export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHeadComponents }) => {
   setHeadComponents([
+    <meta
+      name="google-site-verification"
+      content="9isK6Cy-UKzUynqAIKFHJhvvUUtbwrdusrV1XCrB1Qk"
+      key="googleSiteVerification"
+    />,
     <link
       rel="preload"
       href="/fonts/work-sans.var.woff2"
